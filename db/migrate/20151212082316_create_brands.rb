d3e66ac768a8c9494b14c8dadc45eafe4a009650
Kiryanenko <KiryanenkoAV@mail.ru>
2015-12-12 11:24:24 +0300

    add Brand

---
+++ db/migrate/20151212082316_create_brands.rb
@@ -0,0 +1,11 @@
+class CreateBrands < ActiveRecord::Migration
+  def change
+    create_table :brands do |t|
+      t.string :name
+      t.text :description
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
