23953861877d60fdc90cf1237015cd5c7ce67a3a
Kiryanenko <KiryanenkoAV@mail.ru>
2015-12-18 23:26:56 +0300

    set halls form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,3 @@
 //= require turbolinks
 //= require_tree .
 //= require materialize-sprockets
-
-$(document).ready(function(){
-    $('.collapsible').collapsible({
-        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-});
