bf43d890446596936d09f72ce73892ee453d4832
Kiryanenko <KiryanenkoAV@mail.ru>
2015-12-19 11:50:25 +0300

    fix js x y coords

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,4 +1,9 @@
-function coords(e, scale) { // Вставляет в поля текущие координаты при клике
-    document.getElementById('field_x').value = (event.clientX - e.x + 0.0) / scale
-    document.getElementById('field_y').value = (event.clientY - e.y + 0.0) / scale
+function coords(event, el, scale) { // Вставляет в поля текущие координаты при клике
+    var event = event || window.event;
+    document.getElementById('field_x').value = (event.pageX - el.x + 0.0) / scale
+    document.getElementById('field_y').value = (event.pageY - el.y + 0.0) / scale
+}
+
+function marker(e, x, y) {
+
 }
