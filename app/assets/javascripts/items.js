b06f70fcd8fbbc0aff0cebe906da68e336d9ed14
Kiryanenko <KiryanenkoAV@mail.ru>
2015-12-19 16:59:38 +0300

    add marker to item

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,9 +1,14 @@
 function coords(event, el, scale) { // Вставляет в поля текущие координаты при клике
     var event = event || window.event;
-    document.getElementById('field_x').value = (event.pageX - el.x + 0.0) / scale
-    document.getElementById('field_y').value = (event.pageY - el.y + 0.0) / scale
+    var x = event.pageX - el.x
+    var y = event.pageY - el.y
+    setMarker(x, y);
+    document.getElementById('field_x').value = (x + 0.0) / scale;
+    document.getElementById('field_y').value = (y + 0.0) / scale;
 }
 
-function marker(e, x, y) {
-
+function setMarker(x, y) {
+    var marker = document.getElementById('marker');
+    if (x != undefined) marker.style.left = (x - 12) + 'px';
+    if (y != undefined) marker.style.top  = (y - 24) + 'px';
 }
