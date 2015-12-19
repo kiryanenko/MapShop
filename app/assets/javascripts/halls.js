f5e46627e29214710d3d68947f67d0c61fc32408
Kiryanenko <KiryanenkoAV@mail.ru>
2015-12-19 19:42:30 +0300

    fix marker to halls show

--- app/assets/javascripts/halls.js
+++ app/assets/javascripts/halls.js
@@ -1,6 +1,13 @@
-function setMarker(x, y) {
+function setMarkerHall(x, y) {
     var marker = document.getElementById('marker');
-    marker.hidden = false;
+    marker.style.display = 'block';
     marker.style.left = (x - 12) + 'px';
     marker.style.top  = (y - 24) + 'px';
+}
+
+function mouseOver(el) {
+    el.style.backgroundColor = "cornsilk";
+}
+function mouseOut(el) {
+    el.style.backgroundColor = "white";
 }
